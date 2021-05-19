c88fe05f8bd12ed1397996bb5c682fd7c876da2f
agragland <agragland00@gmail.com>
2021-05-19 00:27:33 -0500

    Attempt at Implementing N-way random covering algorithm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tuple_set import TupleSet
 from test_suite import TestSuite
-import time, re
+import time, re, random
 
 
 def generate_covering_array(re_input):
@@ -22,55 +22,60 @@ def generate_covering_array(re_input):
     return cover_arr
 
 
-def test_suite_output(suite):
-    file_out = open("aetg_output.txt", "w")
+def generate_tuples(new_list, avoid):
+    ret_list = []
 
-    file_out.write(str(len(suite)) + "\n\n")
+    for factor in new_list:
+        if new_list.index(factor) != avoid:
+            ret_list.extend(factor)
 
-    for candidate in suite:
-        file_out.write(str(candidate) + "\n")
-
-    file_out.close()
+    return ret_list
 
 
 def main():
-    # prompt for user input
-    cover_input = input("Input as follows - \"#Levels^#Factors\" - put a space between each for multi-level covering:")
+    test_list = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]
 
-    new_list = generate_covering_array(cover_input)
-    tuples = TupleSet(new_list)
+    suite = []
+    covered_tuples = []
+    while len(covered_tuples) < 54:
 
-    # generate 100 test suites
-    start_time = time.time()
-    test_suites = []
-    for i in range(100):
-        suite = TestSuite(tuples)
-        print(str(i) + " %")
-        test_suites.append(suite.generate_suite())
-    end_time = time.time()
+        candidate_row = []
+        tuples = []
+        for factor in test_list:
+            candidate_set = set()
+            for value in factor:
+                tuples = generate_tuples(test_list, test_list.index(factor))
+                for t in tuples:
+                    if t < value:
+                        selected_tuple = (t, value)
+                        if selected_tuple not in covered_tuples:
+                            candidate_set.add(value)
+                            break
+                    elif t > value:
+                        selected_tuple = (value, t)
+                        if selected_tuple not in covered_tuples:
+                            candidate_set.add(value)
+                            break
+            if len(candidate_set) > 0:
+                selected_value = random.choice(list(candidate_set))
+            for t in tuples:
+                if t < selected_value:
+                    new_tuple = (t, selected_value)
+                    if new_tuple not in covered_tuples and t in candidate_row:
+                        covered_tuples.append(new_tuple)
+                elif selected_value < t:
+                    new_tuple = (selected_value, t)
+                    if new_tuple not in covered_tuples and t in candidate_row:
+                        covered_tuples.append(new_tuple)
+            candidate_row.append(selected_value)
+        suite.append(candidate_row)
 
-    # run statistics
-    lowest_suite = test_suites[0]
-    lowest = len(lowest_suite)
-    highest = 0
 
-    suite_sum = 0
-    for suite in test_suites:
-        if len(suite) > highest:
-            highest = len(suite)
-        if len(suite) < lowest:
-            lowest = len(suite)
-            lowest_suite = suite
-        suite_sum += len(suite)
-
-    print("Results:\n"
-        "Lowest AETG:" + str(lowest) + "\n"
-        "Highest AETG: " + str(highest) + "\n"
-        "Average AETG: " + str(
-        suite_sum / 100) + "\n"
-        "Average Execution Time: " + "{:.6f}".format((end_time-start_time)/100) + "\n")
-
-    test_suite_output(lowest_suite)
+    # input list of of t factors and k levels L_tk = (N; n, t, k)
+    # input event combination strength, n
+    # output n-way covering array
+    for row in suite:
+        print(row)
 
 
 if __name__ == '__main__':
